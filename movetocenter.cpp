8b4c68f019ecfa2a631910056340b98bd9b6f784
Alexey <alexey.smirnoff1985@gmail.com>
2016-12-27 16:58:55 +0300

    27-12-16 update2

---
+++ movetocenter.cpp
@@ -0,0 +1,6 @@
+#include "movetocenter.h"
+
+moveToCenter::moveToCenter(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ movetocenter.h
@@ -0,0 +1,17 @@
+#ifndef MOVETOCENTER_H
+#define MOVETOCENTER_H
+
+#include <QObject>
+
+class moveToCenter : public QObject
+{
+    Q_OBJECT
+public:
+    explicit moveToCenter(QObject *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // MOVETOCENTER_H
